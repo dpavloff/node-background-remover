bd7d8acc1dcff766da046c888f5cde5ee6ba579f
pavlov.dan <pavlov.dan@panoramik.ru>
2021-10-01 20:13:43 +0500

    GET /image/id request finished, added nodemon

--- src/api/getJpg.js
+++ src/api/getJpg.js
@@ -1,6 +1,29 @@
-const db = require('../entities/Database');
-const JPG = require('../entities/JPG');
+const path = require('path');
+const { exists } = require('../utils/fs');
+const { storageFolder } = require('../config/index');
+const { BadRequestApiError } = require('../validators/errors/ApiError');
 
-module.exports = async (req, res) => {
-  const { id } = req.params;
+module.exports = async (req, res, next) => {
+  try {
+    let { id } = req.params;
+
+    if (!id) {
+      throw new BadRequestApiError(
+        'Filename should be provided'
+      );
+    }
+
+    id += '_original.jpg';
+
+    const pathToFile = path.resolve(storageFolder, id);
+    const isFileExists = await exists(pathToFile);
+
+    if (isFileExists === false) {
+      throw new NotFoundApiError('Given JPG file not found');
+    }
+
+    return res.download(pathToFile);
+  } catch (err) {
+    next(err);
+  }
 };
